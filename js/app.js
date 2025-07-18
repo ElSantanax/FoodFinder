3ecf6f12088fcfd513be72bec1bc09d4c284568c
ElSantanax <josesantanaserrano130598@gmail.com>
2025-07-18 11:19:49 -0500

    Leyendo resultados de consultas y scripting para platillos

--- js/app.js
+++ js/app.js
@@ -28,7 +28,29 @@ function iniciarApp() {
         const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`
         fetch(url)
             .then(respuesta => respuesta.json())
-            .then(resultado => console.log(resultado));
+            .then(resultado => mostrarRecetas(resultado.meals));
+    }
+
+    function mostrarRecetas(recetas = []) {
+
+        recetas.forEach(recetas => {
+
+            const { idMeal, strMeal, strMealThumb } = recetas;
+
+            const recetaContenedor = document.createElement('DIV');
+            recetaContenedor.classList.add('col-md-4');
+
+            const recetaCard = document.createElement('DIV');
+            recetaCard.classList.add('card', 'mb-4');
+
+            const recetaImagen = document.createElement('IMG');
+            recetaImagen.classList.add('card-img-top');
+            recetaImagen.alt = `Imagen de la receta ${strMeal}`;
+            recetaImagen.src = strMealThumb;
+
+            const recetaCardBody = document.createElement('DIV');
+            recetaCardBody.classList.add('card-body');
+        });
     }
 }
 
