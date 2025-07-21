1b4942341de80e4d29f74160ef4256fb5b7acefe
ElSantanax <josesantanaserrano130598@gmail.com>
2025-07-21 08:18:35 -0500

    Mostrando ingrediente y cantidades en el modal

--- js/app.js
+++ js/app.js
@@ -105,8 +105,26 @@ function iniciarApp() {
             <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}"> 
             <h3 class="my-3">Instrucciones</h3>
             <p>${strInstructions}</p>
+            <h3 class="my-3">Ingredientes y cantidades</h3>
         `;
 
+        const listGrup = document.createElement('UL');
+        listGrup.classList.add('list-group');
+
+        for (let i = 1; i <= 20; i++) {
+            if (receta[`strIngredient${i}`]) {
+                const ingrediente = receta[`strIngredient${i}`];
+                const cantidad = receta[`strMeasure${i}`];
+
+                const ingredienteLi = document.createElement('LI');
+                ingredienteLi.classList.add('list-group-item');
+                ingredienteLi.textContent = `${ingrediente} - ${cantidad}`;
+
+                listGrup.appendChild(ingredienteLi);
+            }
+        }
+
+        modalBody.appendChild(listGrup);
         modal.show();
     }
 
